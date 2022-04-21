14055a8aca4a8e3d88b5c96f1fc1e53148889e0a
Altvir <ilyas.abdrasilov@mail.ru>
2022-04-21 20:42:46 +0600

    AA

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from "./components/Header";
+import Navb from "./components/Navb";
+import Content from "./components/Content";
+import Footer from "./components/Footer";
 
-function App() {
+
+
+
+const App=()=> {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      // eslint-disable-next-line react/react-in-jsx-scope
+   <div className='app-wrapper'>
+
+       {/* eslint-disable-next-line react/react-in-jsx-scope */}
+    <Header />
+       {/* eslint-disable-next-line react/react-in-jsx-scope */}
+    <Navb />
+       {/* eslint-disable-next-line react/react-in-jsx-scope */}
+    <Content />
+       {/* eslint-disable-next-line react/react-in-jsx-scope */}
+       <Footer />
+   </div>
   );
 }
 
